c4070e7e04208167807995801b0f90f0dc232c73
ddddq187 <ddddq187@gmail.com>
2026-01-21 15:05:51 +0800

    feat: :art: print pte

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,25 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void recur_vmprint(pagetable_t pt,int depth){
+  for (int i=0; i<512; ++i) {
+    pte_t pte = pt[i];
+    
+    if (pte & PTE_V) {
+      printf("..");
+      for (int j=0; j<depth; ++j) {
+        printf("..");
+      }
+      printf("%d: pte %p pa %p\n",i, pte, PTE2PA(pte));
+      if ((pte & PTE_V) && (pte & (PTE_R | PTE_W | PTE_X))==0) {
+        uint64 child =PTE2PA(pte);
+        recur_vmprint((pagetable_t)child,depth+1);
+      }
+    }
+  }
+}
+void vmprint(pagetable_t pt){
+  printf("page table %p\n",pt);
+  recur_vmprint(pt,0);
+}
